96526929b0daa6dc390b6b674362914859d68121
NEXON\jhyounyaho <jhyoun-yaho@hanmail.net>
2021-01-27 14:32:21 +0900

    pokelist API 변경

--- src/api/index.js
+++ src/api/index.js
@@ -4,8 +4,8 @@ const config = {
     baseUrl : 'https://pokeapi.co/api/v2/pokemon',
 }
 
-function fetchPokeList(limit) {
-    return axios.get(`${config.baseUrl}?limit=${limit}`)
+function fetchPokeList(offset) {
+    return axios.get(`${config.baseUrl}?offset=${offset}limit=20`)
 }
 
 function fetchPokeInfo(id) {
