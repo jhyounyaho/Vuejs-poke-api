0aecbd03dfc5437a1cded022a42f3588850ee903
NEXON\jhyounyaho <jhyoun-yaho@hanmail.net>
2021-01-27 18:14:21 +0900

    스크랩 기능 - 스크랩 여부 노출, 스크랩 추가/삭제,

--- src/components/Poke.vue
+++ src/components/Poke.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <i class="fas fa-heart" @click="pokeScrap(pokeId)"></i>
+    <i class="fas fa-heart" @click="pokeScrap(pokeId)" :class="{ isScrap: isScrap }"></i>
     <img :src="`${imgSrc}${pokeId}.png`" @click="pokeDetail(pokeId)">
     <p>{{ this.pokeName }}</p>
   </div>
@@ -19,7 +19,12 @@ export default {
       pokeId: this.id,
       pokeName: this.name,
       imgSrc: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/',
+      isScrap: false,
     }
+  },
+  created() {
+    // 스크랩 여부 체크
+    this.isPokeScrap(this.pokeId)
   },
   methods: {
     ...mapActions([
@@ -28,9 +33,33 @@ export default {
     pokeDetail(id) {
       this.FETCH_POKE_INFO(id);
     },
-    pokeScrap(id) {
-      console.log(id)
+    isPokeScrap(id) {
+      const storageScrapList = this.parsingScrapList();
+      const findScrap = storageScrapList.find((scrapId) => scrapId === id);
+      this.isScrap = findScrap ? true : false;
     },
+    pokeScrap(id) {
+      !this.isScrap ? this.addPokeScrap(id) : this.deletePokeScrap(id)
+    },
+    addPokeScrap(id) {
+      const storageScrapList = this.parsingScrapList();
+      if (storageScrapList) {
+        const findScrap = storageScrapList.find(scrapId => scrapId === id);
+        if (findScrap) return false
+      }
+      storageScrapList.push(id)
+      localStorage.scraplist = JSON.stringify(storageScrapList);
+      this.isScrap = true
+    },
+    deletePokeScrap(id) {
+      const storageScrapList = this.parsingScrapList();
+      const updateScrapList = storageScrapList.filter(scrapId => scrapId !== id)
+      localStorage.scraplist = JSON.stringify(updateScrapList);
+      this.isScrap = false
+    },
+    parsingScrapList() {
+      return localStorage.scraplist ? JSON.parse(localStorage.scraplist) : [];
+    }
   }
 }
 </script>
@@ -44,5 +73,8 @@ i {
   position: absolute;
   padding: 10px;
   display: block;
+}
+.isScrap {
+  color: #f03e3e;
 }
 </style>
